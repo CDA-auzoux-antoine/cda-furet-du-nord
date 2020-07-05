f483993f1b32a766cccecd003108d7bc848475e3
CDA-Benseddik-Fethi <58114380+CDA-Benseddik-Fethi@users.noreply.github.com>
2020-07-05 15:02:27 +0200

    idaocmd

--- cda-librairie-v2/src/cda/librairie/dao/DaoCommande.java
+++ cda-librairie-v2/src/cda/librairie/dao/DaoCommande.java
@@ -9,31 +9,30 @@ public class DaoCommande implements IDao<Commande> {
 
 	@Override
 	public Commande create(Commande t) {
-		// TODO Auto-generated method stub
+
 		return null;
 	}
 
 	@Override
 	public Commande update(Commande t) {
-		// TODO Auto-generated method stub
+
 		return null;
 	}
 
 	@Override
 	public void delete(Commande t) {
-		// TODO Auto-generated method stub
 
 	}
 
 	@Override
 	public Commande find(int id) {
-		// TODO Auto-generated method stub
+
 		return null;
 	}
 
 	@Override
 	public List<Commande> selectAll() {
-		// TODO Auto-generated method stub
+
 		return null;
 	}
 
